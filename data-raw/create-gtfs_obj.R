e47f8d13ed511544ce31343d2bcfea66390f4a2a
Tom Buckley <buckley.tom@gmail.com>
2018-07-14 17:06:55 -0700

    add mock gtfs data
    this went missing or wasn't included, meaning some examples couldn't run

--- data-raw/create-gtfs_obj.R
+++ data-raw/create-gtfs_obj.R
@@ -1,5 +1,6 @@
 devtools::load_all()
 url <- "http://data.trilliumtransit.com/gtfs/duke-nc-us/duke-nc-us.zip"
-gtfs_obj <- url %>% import_gtfs(quiet=TRUE)
+gtfs_obj <- import_gtfs(url, quiet=TRUE)
 saveRDS(gtfs_obj, here::here('data-raw/gtfs_obj'))
+devtools::use_data(gtfs_obj)
 
